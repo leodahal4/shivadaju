46b8cb8816d993fe28714159b6c3e88877cc5677
leodahal4@gmail.com <leodahal4@gmail.com>
2022-03-28 18:15:37 +0545

    again monir changes

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
-  // publicPath: "/shivadaju/",
+  publicPath: "/shivadaju/",
 });
